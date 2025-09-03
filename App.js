ccedb719f4f519bbd30007504cdc3b6159961c2a
Sony <143217009+SomethingWithSony@users.noreply.github.com>
2025-09-03 18:26:29 +0100

    Update App.js bringing all the components together

--- App.js
+++ App.js
@@ -1,151 +1,61 @@
-import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, FlatList } from 'react-native';
+import { ImageBackground, SafeAreaView, StyleSheet} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { useState } from 'react';
+import Colors from './utils/colors';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
-  const [userInput, setUserInput] = useState('');
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [userNumber, setUserNumber] = useState(null);
+  const [isGameOver, setIsGameOver] = useState(true);
+  const [numberOfGuesses, setNumberOfGuesses] = useState(1);
+
   
-  const [opponentGuess, setOpponentGuess] = useState('');
-  const [opponentGuessHistory, setOpponentGuessHistory] = useState([]);
-
-  const [lastMin , setLastMin] = useState(0);
-  const [lastMax, setLastMax] = useState(99)
-
-  const reset = () => {
-    setUserInput('');
+  const startGameHandler = (pickedNumber) => {
+    setUserNumber(pickedNumber)
+    setIsGameOver(false)
   }
 
-  const handleTextChange = (text) => {
-    if (!isNaN(text) && text.length <= 2) {
-      setUserInput(text);
-    }
+  let screen = <StartGameScreen onConfirmNumber={startGameHandler} />
+
+  const gameOverHandler = () => {
+    setIsGameOver(true)
   }
 
-  const confirm = () => {
-    setIsPlaying(true)
-    let guess = randomIntFromInterval(lastMin, lastMax) ;
-    setOpponentGuess(guess);
-    console.log("Player input" + userInput);
-    detectVictory();
+  const newGameHandler = () => {
+    setUserNumber(null);
+    setNumberOfGuesses(1);
   }
 
-  // Aux Function
-  function randomIntFromInterval(min, max) { 
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  console.log(numberOfGuesses)
+
+  if (userNumber) {
+    screen = (<GameScreen userNumber={userNumber} onGameOver={gameOverHandler} onGuess={(currentGuessNumber) => setNumberOfGuesses(numberOfGuesses + 1)}/>)
   }
 
-  const lower = () => {
-    if (opponentGuess < userInput){
-      console.log("No no you cheating")
-    } else {
-      setOpponentGuessHistory( currentHistory => [...currentHistory, {id: opponentGuessHistory.length, data: opponentGuess}]);
-      console.log("guessing a lower num: " + opponentGuess + " user input: " + userInput);
-      setLastMax(opponentGuess - 1)
-      let guess = randomIntFromInterval(lastMin, opponentGuess);
-      console.log("range : "  + lastMin +  " - " + opponentGuess)
-      setOpponentGuess(guess);
-      detectVictory(guess);
-
-    }
-    console.log(opponentGuessHistory)
-
+  if (isGameOver && userNumber){
+    screen = (<GameOverScreen userNumber={userNumber} onNewGame={newGameHandler} numberOfRounds={numberOfGuesses}/>)
   }
 
-  const higher = () => {
-    if (opponentGuess > userInput){
-      console.log("No no you cheating");
-    } else {
-      setOpponentGuessHistory( currentHistory => [...currentHistory, {id: opponentGuessHistory.length, data: opponentGuess}])
-      console.log("guessing a higher num: " + opponentGuess + " user input: " + userInput);
-      setLastMin(opponentGuess + 1)
-      let guess = randomIntFromInterval(opponentGuess, lastMax);
-      console.log("range : "  + opponentGuess +  " - " + lastMax)
-      setOpponentGuess(guess);
-      detectVictory(guess);
-    }
-
-
-    
-  }
-
-  const detectVictory = (guess) => {
-    if (guess == userInput){
-      setIsPlaying(false);
-      setUserInput('');
-      setOpponentGuessHistory([]);
-      setLastMin(0);
-      setLastMax(99);
-      setOpponentGuess('')
-      console.log("Computer Won");
-
-    }
-  }
-
-  return (
-    <SafeAreaView style={styles.container}>
-      { !isPlaying ?
-      <View>
-        <View>
-          <Text>Guess My Number</Text>
-        </View>
-
-        <View>
-          <Text>Enter a Number</Text>
-
-          <TextInput placeholder='00'
-            value={userInput}
-            keyboardType='numeric'
-            onChangeText={handleTextChange}
-          />
-
-          <View>
-            <Button title='Reset' onPress={reset} />
-            <Button title='Confirm' onPress={confirm} />
-          </View>
-        </View>
-      </View>
-      : null
-      
-    }
-
-    {
-      isPlaying ?
-      <View>
-        <View>
-          <Text>Opponent's Guess</Text>
-        </View>
-        <View>
-          <Text>{opponentGuess}</Text>
-        </View>
-        <View>
-            <Text>Higher or lower?</Text>
-            <View>
-              <Button title='-' onPress={lower}/>
-              <Button title='+' onPress={higher}/> 
-            </View>
-        </View>
-        <View>
-          <FlatList  
-            data={opponentGuessHistory}
-            renderItem={ itemData => {
-              return (
-                  <Text>#{itemData.item.id}  Opponent's Guess: {itemData.item.data} </Text> 
-              )
-            }}
-          />
-        </View>
-      </View> : null
-    }
-
-    </SafeAreaView>
+  
+  
+  return ( 
+    <LinearGradient style={styles.rootScreen} colors={[Colors.primary500, Colors.secondary]}> 
+      <ImageBackground source={require('./assets/background.png')} resizeMode="cover" style={styles.rootScreen} imageStyle={styles.backgroundImg}>
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  backgroundImg : {
+    opacity: 0.15
+ 
+}
+
 });
